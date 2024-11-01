61bf33097534573fd9791911bdb15d2e62b0ea52
FinleyConway <finleyconway03@gmail.com>
2024-11-01 16:15:12 +0000

    start of replacing visual studio build

--- Minesweeper/src/SfmlHandler.cpp
+++
@@ -1,9 +0,0 @@
-#include "SfmlHandler.hpp"
-
-namespace Minesweeper {
-
-	SfmlHandler::SfmlHandler(const std::string& title, uint32_t width, uint32_t height) :
-		m_Window(sf::VideoMode(width, height), title, sf::Style::Close | sf::Style::Titlebar)
-	{
-	}
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Minesweeper
-![image](https://user-images.githubusercontent.com/79692910/231227319-d6209dc8-5c2e-441d-b915-3105e0125b9c.png)
